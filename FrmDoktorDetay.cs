b3563f0e8193049d661dae5bd126cc6d1d1d5a37
tugba <tugbanmdr@outlook.com>
2024-05-24 13:04:29 +0300

    first commit
    aster

---
+++ FrmDoktorDetay.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Proje_Hastane
+{
+    public partial class FrmDoktorDetay : Form
+    {
+        public FrmDoktorDetay()
+        {
+            InitializeComponent();
+        }
+        sqlbaglantisi bgl = new sqlbaglantisi();
+        public string TC;
+
+        private void FrmDoktorDetay_Load(object sender, EventArgs e)
+        {
+            LblTC.Text = TC;
+            //Doktor AD VE SOYAD 
+            SqlCommand komut = new SqlCommand("Select DoktorAd,DoktorSoyad From Tbl_Doktorlar where DoktorTC=@p1", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", LblTC.Text);
+
+            SqlDataReader dr = komut.ExecuteReader();
+            while(dr.Read())
+            {
+                LblAdSoyad.Text = dr[0] + " " + dr[1];
+            }
+            bgl.baglanti().Close();
+            //Randevular
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select * From Tbl_Randevular where RandevuDoktor= '" + LblAdSoyad.Text +"'", bgl.baglanti());
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+      
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+            DataTable dt1 = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select  From Tbl_Randevular", bgl.baglanti());
+            da.Fill(dt1);
+            dataGridView1.DataSource = dt1;
+
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            FrmDoktorBilgiDuzenle fr = new FrmDoktorBilgiDuzenle();
+            fr.TCNO = LblTC.Text;
+            fr.Show();
+        }
+
+        private void BtnDuyurular_Click(object sender, EventArgs e)
+        {
+            FrmDuyurular fr = new FrmDuyurular();
+            fr.Show();
+        }
+
+        private void BtnCikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmDoktorGiris fr = new FrmDoktorGiris();
+            fr.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ FrmRandevuListesi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Proje_Hastane
+{
+    public partial class FrmRandevuListesi : Form
+    {
+        public FrmRandevuListesi()
+        {
+            InitializeComponent();
+        }
+
+        sqlbaglantisi bgl = new sqlbaglantisi();
+        private void FrmRandevuListesi_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select * From Tbl_Randevular", bgl.baglanti());
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
